7772a3da078fd46b0a737b74721f84b220bc7bfb
José Mateus <43993359+JoseMateusss@users.noreply.github.com>
2022-10-10 16:24:09 -0300

    Add User and Category Fk's in Migration

--- database/migrations/2022_10_10_183212_create_tasks_table.php
+++ database/migrations/2022_10_10_183212_create_tasks_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\models\User;
+use App\models\Category;
 
 return new class extends Migration
 {
@@ -18,7 +20,8 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->dateTime('due_date');
-            
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreignIdFor(Category::class)->references('id')->on('categories')->onDelete('CASCADE');
             $table->timestamps();
         });
     }
@@ -30,6 +33,10 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::table('tasks', function (Blueprint $table){
+            $table->dropForeignIdFor(User::class);
+            $table->dropForeignIdFor(Category::class);
+        });
         Schema::dropIfExists('tasks');
     }
 };
